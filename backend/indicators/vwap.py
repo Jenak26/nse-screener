c646caab91fc27ca392c0f59a7dbf60d7c198ebb
Janak <janaak2617@gmail.com>
2026-05-22 17:58:06 +0530

    chore: remove old Streamlit/pattern code, scaffold new backend structure

--- backend/indicators/vwap.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def calculate_vwap(candles: pd.DataFrame) -> pd.Series:
-    """Calculate VWAP from a candles DataFrame. Assumes intraday session."""
-    typical_price = (candles["high"] + candles["low"] + candles["close"]) / 3
-    cumulative_tp_vol = (typical_price * candles["volume"]).cumsum()
-    cumulative_vol = candles["volume"].cumsum()
-    vwap = cumulative_tp_vol / cumulative_vol
-    return vwap
